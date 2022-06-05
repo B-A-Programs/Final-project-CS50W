62b450eb39b8a14552e91ac9b397615c7ab9a641
B-A-Programs <86296428+B-A-Programs@users.noreply.github.com>
2022-06-05 15:31:04 +0300

    Update models.py

--- JobFinder/JobFinderApp/models.py
+++ JobFinder/JobFinderApp/models.py
@@ -3,4 +3,24 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    description = models.CharField(max_length=500, blank=True)
+
+class job_experiences(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="jobs")
+    company_name = models.CharField(max_length=50)
+    position = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class languages(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="languages")
+    language = models.CharField(max_length=50)
+    level = models.CharField(max_length=50)
+
+class education(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="education")
+    institution = models.CharField(max_length=50)
+    level = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
