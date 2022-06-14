95f26c341a2b1e1c0af6e43077b568f0d70f6d94
Andrei Tudorache <tudoracheandrei09@gmail.com>
2022-06-14 14:41:46 +0300

    Finished index page

---
+++ JobFinder/JobFinderApp/static/JobFinderApp/index.js
@@ -0,0 +1,24 @@
+// Create dropdowns
+document.addEventListener('DOMContentLoaded', function() {
+    var select1 = document.getElementById("dp_people");
+    var options = ["Architecture and engineering", "Arts, culture and entertainment", "Business, management and administration", "Communications", "Community and social services",
+        "Education", "Science and technology", "Installation, repair and maintenance", "Farming, fishing and forestry", "Health and medicine", "Law and public policy", "Sales"];
+
+    for(var i = 0; i < options.length; i++) {
+        var x = options[i];
+        var op = document.createElement('a');
+        op.textContent = x;
+        op.href = `users/${x}`
+        op.class = 'dropdown-content'
+        select1.appendChild(op);
+    }
+
+    var select2 = document.getElementById("dp_jobs")
+    for(var i = 0; i < options.length; i++) {
+        var x = options[i];
+        var op = document.createElement('a');
+        op.textContent = x;
+        op.href = `posts/${x}`
+        select2.appendChild(op);
+    }
+})
